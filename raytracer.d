ac700347b941bd7bf1a97cf0f6c0c8f6c8d89464
Jamie Border <jamie.border@uq.net.au>
2019-07-10 20:59:28 +1000

    ch2: done

--- raytracer.d
+++ raytracer.d
@@ -1,22 +1,29 @@
 import std.stdio;
 import std.conv: to;
-import std.file: append;
+import std.file: append, remove;
+
+import vector;
 
 void main()
 {
     string outputFile = "./data.ppm";
+    try {
+        remove(outputFile);
+    }
+    finally {}
+
     int nx = 200;
     int ny = 100;
     append(outputFile, "P3\n "~to!string(nx)~" "~to!string(ny)~"\n255\n");
     
     for (int j = ny-1; j >=0; j--) {
         for (int i = 0; i <nx; i++) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
-            int ir = to!int(255.99 * r);
-            int ig = to!int(255.99 * g);
-            int ib = to!int(255.99 * b);
+            Vector3 col = Vector3(float(i) / float(nx),
+                                  float(j) / float(ny),
+                                  0.2);
+            int ir = to!int(255.99 * col.r);
+            int ig = to!int(255.99 * col.g);
+            int ib = to!int(255.99 * col.b);
 
             append(outputFile, to!string(ir)~" "
                               ~to!string(ig)~" "
---
+++ vector.d
@@ -0,0 +1,89 @@
+import std.stdio;
+import std.math;
+
+// alias inline() = pragma(inline, true);
+// alias pragma(inline, true) inline;
+
+struct Vector3
+{
+public:
+    float[3] e;
+
+    this (float e0, float e1, float e2)
+    {
+        e[0] = e0;
+        e[1] = e1;
+        e[2] = e2;
+    }
+
+    pragma(inline, true) float x() const { return e[0]; }
+    pragma(inline, true) float y() const { return e[1]; }
+    pragma(inline, true) float z() const { return e[2]; }
+    pragma(inline, true) float r() const { return e[0]; }
+    pragma(inline, true) float g() const { return e[1]; }
+    pragma(inline, true) float b() const { return e[2]; }
+
+    pragma(inline, true) float* opUnary(string s)() if (s == "+") 
+    { 
+        return &this; 
+    }
+    pragma(inline, true) Vector3 opUnary(string s)() if (s == "-") 
+    { 
+        return Vector3(-e[0], -e[1], -e[2]); 
+    }
+
+    pragma(inline, true) float opIndex(size_t i) { return e[i]; }
+    pragma(inline, true) float* opIndex(size_t i) { return &e[i]; }
+
+    pragma(inline, true) Vector3 opBinary(string op)(const Vector3* rhs)
+    { 
+        return mixin("Vector3(e[0] "~op~" rhs.e[0],"
+                            ~"e[1] "~op~" rhs.e[1],"
+                            ~"e[2] "~op~" rhs.e[2]);");
+    }
+
+    pragma(inline, true) Vector3 opBinary(string op)(const float scalar)
+    {
+        return mixin("Vector3(e[0] "~op~" scalar,"
+                            ~"e[1] "~op~" scalar,"
+                            ~"e[2] "~op~" scalar);");
+    }
+
+    pragma(inline, true) void opOpAssign(string op)(const Vector3 rhs)
+    {
+        mixin("e[0] "~op~"= rhs.e[0];"
+             ~"e[1] "~op~"= rhs.e[1];"
+             ~"e[2] "~op~"= rhs.e[2];");
+    }
+
+    pragma(inline, true) void opOpAssign(string op)(const float scalar)
+        if (op == "*" || op == "/")
+    {
+        mixin("e[0] "~op~"= scalar;"
+             ~"e[1] "~op~"= scalar;"
+             ~"e[2] "~op~"= scalar;");
+    }
+    
+    pragma(inline, true) float dot(const Vector3* rhs)
+    {
+        return e[0] * rhs.e[0] + e[1] * rhs.e[1] + e[2] * rhs.e[2];
+    }
+
+    pragma(inline, true) Vector3 cross(const Vector3* rhs)
+    {
+        return Vector3(e[1] * rhs.e[2] - e[2] * rhs.e[1],
+                       e[2] * rhs.e[0] - e[0] * rhs.e[2],
+                       e[0] * rhs.e[1] - e[1] * rhs.e[0]);
+    }
+
+    pragma(inline, true) void makeUnitVector()
+    {
+        float k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+        e[0] *= k; e[1] *= k; e[2] *= k;
+    }
+
+    pragma(inline, true) Vector3 unitVector()
+    {
+        return Vector3(e[0] / e.length, e[1] / e.length, e[2] / e.length);
+    }
+}
